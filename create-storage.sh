9eb3a9d6ce9432bcc2949bd5d2ee4a66d6428ac4
progzilla <oladipupo.popoola@gmail.com>
2021-11-29 15:54:18 +0000

    updated blob service

--- create-storage.sh
+++ create-storage.sh
@@ -1,11 +1,11 @@
 #Create RG for storing State Files
-az group create --location westus2 --name rg-terraformstate
+az group create --location uksouth --name rg-terraformstate
 
 #Create Storage Account
-az storage account create --name terrastatestorage2021 --resource-group rg-terraformstate --location westus2 --sku Standard_LRS
+az storage account create --name terrastatestorage2022 --resource-group rg-terraformstate --location uksouth --sku Standard_LRS
 
 #Create Storage Container
-az storage container create --name terraformdemo --account-name terrastatestorage2021
+az storage container create --name terraformdemo --account-name terrastatestorage2022
 
 #Enable versioning on Storage Account1
-az storage account blob-service-properties update --account-name terrastatestorage2021 --enable-change-feed --enable-versioning true
+az storage account blob-service-properties update --resource-group rg-terraformstate --account-name terrastatestorage2022 --enable-change-feed --enable-versioning true
