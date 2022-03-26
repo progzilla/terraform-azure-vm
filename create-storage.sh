719551e9d5249204ae6e89033e7f80fadc712482
progzilla <oladipupo.popoola@gmail.com>
2022-03-26 01:15:46 +0000

    modified resources identifier

--- create-storage.sh
+++ create-storage.sh
@@ -1,11 +1,11 @@
 #Create RG for storing State Files
-az group create --location uksouth --name rg-terraformstate
+az group create --location uksouth --name rg-terrastate
 
 #Create Storage Account
-az storage account create --name terrastatestorage2022 --resource-group rg-terraformstate --location uksouth --sku Standard_LRS
+az storage account create --name terrastatestorage032022 --resource-group rg-terrastate --location uksouth --sku Standard_LRS
 
 #Create Storage Container
-az storage container create --name terraformdemo --account-name terrastatestorage2022
+az storage container create --name terrademo --account-name terrastatestorage032022
 
 #Enable versioning on Storage Account1
-az storage account blob-service-properties update --resource-group rg-terraformstate --account-name terrastatestorage2022 --enable-change-feed --enable-versioning true
+az storage account blob-service-properties update --resource-group rg-terrastate --account-name terrastatestorage032022 --enable-change-feed --enable-versioning true
